c7990390ea6cef1ed62c8e3a60455929f91ec226
PuriPuri <aliceicelie@gmail.com>
2023-07-22 14:04:27 +0300

    fix update feeds

--- src/js/view.js
+++ src/js/view.js
@@ -1,52 +1,52 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import onChange from 'on-change';
-import i18next from 'i18next';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import onChange from "on-change";
+import i18next from "i18next";
 
 const translate = (bible, key) => bible.t(key);
 
 export function closeModal(state, modal) {
-  if (state.urlForm.modal.status === 'opened') {
-    modal.classList.remove('show');
+  if (state.urlForm.modal.status === "opened") {
+    modal.classList.remove("show");
   }
 }
 
 const createCardElement = () => {
-  const card = document.createElement('div');
-  card.classList.add('card', 'border-0');
+  const card = document.createElement("div");
+  card.classList.add("card", "border-0");
   return card;
 };
 
 const createCardBodyElement = () => {
-  const cardBody = document.createElement('div');
-  cardBody.classList.add('card-body');
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
   return cardBody;
 };
 
 const createCardTitleElement = (text) => {
-  const cardTitle = document.createElement('h2');
-  cardTitle.classList.add('card-title', 'h4');
+  const cardTitle = document.createElement("h2");
+  cardTitle.classList.add("card-title", "h4");
   cardTitle.textContent = text;
   return cardTitle;
 };
 
 const createFeedListElement = () => {
-  const ul = document.createElement('ul');
-  ul.classList.add('list-group', 'border-0', 'rounded-0');
+  const ul = document.createElement("ul");
+  ul.classList.add("list-group", "border-0", "rounded-0");
   return ul;
 };
 
 const createFeedListItemElement = (feedTitle, feedDescription) => {
-  const li = document.createElement('li');
-  li.classList.add('list-group-item', 'border-0', 'border-end-0');
+  const li = document.createElement("li");
+  li.classList.add("list-group-item", "border-0", "border-end-0");
   li.dataset.title = feedTitle;
 
-  const h3 = document.createElement('h3');
-  h3.classList.add('h6', 'm-0');
+  const h3 = document.createElement("h3");
+  h3.classList.add("h6", "m-0");
   h3.textContent = feedTitle;
 
-  const p = document.createElement('p');
-  p.classList.add('m-0', 'small', 'text-black-50');
+  const p = document.createElement("p");
+  p.classList.add("m-0", "small", "text-black-50");
   p.textContent = feedDescription;
 
   li.appendChild(h3);
@@ -56,7 +56,7 @@ const createFeedListItemElement = (feedTitle, feedDescription) => {
 };
 
 const createFeedItem = (feeds, feedTitle, feedDescription) => {
-  const ul = feeds.querySelector('ul');
+  const ul = feeds.querySelector("ul");
   const existingFeed = ul.querySelector(`li[data-title="${feedTitle}"]`);
   if (!existingFeed) {
     const li = createFeedListItemElement(feedTitle, feedDescription);
@@ -67,7 +67,7 @@ const createFeedItem = (feeds, feedTitle, feedDescription) => {
 const createFeedSection = (feeds, feedTitle, feedDescription) => {
   const cardFeed = createCardElement();
   const cardBodyFeed = createCardBodyElement();
-  const cardTitleFeed = createCardTitleElement('Фиды');
+  const cardTitleFeed = createCardTitleElement("Фиды");
 
   cardBodyFeed.appendChild(cardTitleFeed);
   cardFeed.appendChild(cardBodyFeed);
@@ -82,51 +82,51 @@ const createFeedSection = (feeds, feedTitle, feedDescription) => {
 
 const createInitialPostsSection = (posts, cardPost, cardTitlePost) => {
   posts.insertBefore(cardPost, cardTitlePost.nextSibling);
-  const ul = document.createElement('ul');
-  ul.classList.add('list-group', 'border-0', 'rounded-0');
+  const ul = document.createElement("ul");
+  ul.classList.add("list-group", "border-0", "rounded-0");
   posts.insertBefore(ul, cardPost.nextSibling);
 };
 
 const createButton = (postTitle, postBody, postLink) => {
-  const button = document.createElement('button');
-  button.type = 'button';
-  button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-  button.setAttribute('data-title', postTitle);
-  button.setAttribute('data-body', postBody);
-  button.setAttribute('data-link', postLink);
-  button.setAttribute('data-bs-toggle', 'modal');
-  button.setAttribute('data-bs-target', '#modal');
-  button.textContent = 'Просмотр';
+  const button = document.createElement("button");
+  button.type = "button";
+  button.classList.add("btn", "btn-outline-primary", "btn-sm");
+  button.setAttribute("data-title", postTitle);
+  button.setAttribute("data-body", postBody);
+  button.setAttribute("data-link", postLink);
+  button.setAttribute("data-bs-toggle", "modal");
+  button.setAttribute("data-bs-target", "#modal");
+  button.textContent = "Просмотр";
   return button;
 };
 
 const createLink = (post, postTitle, index) => {
-  const link = document.createElement('a');
-  link.href = post.querySelector('link').textContent;
-  link.classList.add('fw-bold');
-  link.setAttribute('data-id', index);
-  link.target = '_blank';
-  link.rel = 'noopener noreferrer';
+  const link = document.createElement("a");
+  link.href = post.querySelector("link").textContent;
+  link.classList.add("fw-bold");
+  link.setAttribute("data-id", index);
+  link.target = "_blank";
+  link.rel = "noopener noreferrer";
   link.textContent = postTitle;
-  link.setAttribute('data-title', postTitle);
-  link.setAttribute('data-link', post.querySelector('link').textContent);
+  link.setAttribute("data-title", postTitle);
+  link.setAttribute("data-link", post.querySelector("link").textContent);
   link.setAttribute(
-    'data-description',
-    post.querySelector('description').textContent,
+    "data-description",
+    post.querySelector("description").textContent
   );
   return link;
 };
 
 const getPostData = (post) => {
-  const postBody = post.querySelector('description').textContent;
-  const postLink = post.querySelector('link').textContent;
+  const postBody = post.querySelector("description").textContent;
+  const postLink = post.querySelector("link").textContent;
   return { postBody, postLink };
 };
 
 export const getModalData = (buttonn) => {
-  const titlee = buttonn.getAttribute('data-title');
-  const body = buttonn.getAttribute('data-body');
-  const linkk = buttonn.getAttribute('data-link');
+  const titlee = buttonn.getAttribute("data-title");
+  const body = buttonn.getAttribute("data-body");
+  const linkk = buttonn.getAttribute("data-link");
   return { titlee, body, linkk };
 };
 
@@ -137,15 +137,15 @@ const appendElements = (parent, elements) => {
 };
 
 const createPostItem = (posts, post, postTitle, index) => {
-  const ul = posts.querySelector('ul');
-  const li = document.createElement('li');
+  const ul = posts.querySelector("ul");
+  const li = document.createElement("li");
   li.classList.add(
-    'list-group-item',
-    'd-flex',
-    'justify-content-between',
-    'align-items-start',
-    'border-0',
-    'border-end-0',
+    "list-group-item",
+    "d-flex",
+    "justify-content-between",
+    "align-items-start",
+    "border-0",
+    "border-end-0"
   );
   li.dataset.title = postTitle;
 
@@ -158,8 +158,8 @@ const createPostItem = (posts, post, postTitle, index) => {
 };
 
 const createPosts = (state) => {
-  const posts = document.querySelector('.posts');
-  const cardTitlePost = createCardTitleElement('Посты');
+  const posts = document.querySelector(".posts");
+  const cardTitlePost = createCardTitleElement("Посты");
   posts.insertBefore(cardTitlePost, posts.firstChild);
 
   const cardPost = createCardElement();
@@ -174,7 +174,7 @@ const createPosts = (state) => {
 
   const postArray = state.urlForm.receivedData.posts;
   for (let i = 0; i < postArray.length; i += 1) {
-    const postTitle = postArray[i].querySelector('title').textContent;
+    const postTitle = postArray[i].querySelector("title").textContent;
     const existingPost = posts.querySelector(`a[data-title='${postTitle}']`);
     if (!existingPost) {
       createPostItem(posts, postArray[i], postTitle, i, postArray);
@@ -183,7 +183,7 @@ const createPosts = (state) => {
 };
 
 const createFeeds = (state) => {
-  const feeds = document.querySelector('.feeds');
+  const feeds = document.querySelector(".feeds");
   const feedTitle = state.urlForm.receivedData.feeds[0].textContent;
   const feedDescription = state.urlForm.receivedData.feeds[1].textContent;
 
@@ -196,47 +196,55 @@ const createFeeds = (state) => {
 };
 
 const resetInput = (input) => {
-  input.setAttribute('type', 'reset');
-  input.setAttribute('type', 'search');
-  input.classList.remove('is-valid', 'is-invalid');
-  input.classList.remove('is-valid');
+  input.setAttribute("type", "reset");
+  input.setAttribute("type", "search");
+  input.classList.remove("is-valid", "is-invalid");
+  input.classList.remove("is-valid");
 };
 
-const updateFormStatus = (status, feedback, input, customI18next, customTranslate, state, linky) => {
+const updateFormStatus = (
+  status,
+  feedback,
+  input,
+  customI18next,
+  customTranslate,
+  state,
+  linky
+) => {
   const { receivedData } = state.urlForm;
 
   switch (status) {
-    case 'correct':
-      feedback.classList.remove('text-success', 'text-danger');
-      input.classList.remove('is-valid', 'is-invalid');
-      input.classList.add('is-valid');
-      feedback.classList.add('text-success');
-      feedback.textContent = customTranslate(customI18next, 'correct');
+    case "correct":
+      feedback.classList.remove("text-success", "text-danger");
+      input.classList.remove("is-valid", "is-invalid");
+      input.classList.add("is-valid");
+      feedback.classList.add("text-success");
+      feedback.textContent = customTranslate(customI18next, "correct");
       resetInput(input);
       if (receivedData.feeds.length) {
         createFeeds(state);
       }
       if (receivedData.posts.length) {
         createPosts(state);
-        const links = document.querySelectorAll('[data-id]');
-        links.forEach(item => {
+        const links = document.querySelectorAll("[data-id]");
+        links.forEach((item) => {
           if (linky.title.includes(item.textContent)) {
-            item.classList.remove('fw-bold');
-            item.classList.add('fw-normal', 'link-secondary');
+            item.classList.remove("fw-bold");
+            item.classList.add("fw-normal", "link-secondary");
           }
         });
       }
-      input.value = '';
+      input.value = "";
       break;
-    case 'error':
-    case 'dublicate':
-    case 'parseErr':
-    case 'networkErr':
+    case "error":
+    case "dublicate":
+    case "parseErr":
+    case "networkErr":
       feedback.textContent = customTranslate(customI18next, status);
-      feedback.classList.add('text-danger');
-      input.classList.add('is-invalid');
-      input.classList.remove('is-valid');
-      feedback.classList.remove('text-success');
+      feedback.classList.add("text-danger");
+      input.classList.add("is-invalid");
+      input.classList.remove("is-valid");
+      feedback.classList.remove("text-success");
       break;
     default:
       break;
@@ -244,8 +252,8 @@ const updateFormStatus = (status, feedback, input, customI18next, customTranslat
 };
 
 export const render = (state) => {
-  const input = document.querySelector('#url-input');
-  const feedback = document.querySelector('.feedback');
+  const input = document.querySelector("#url-input");
+  const feedback = document.querySelector(".feedback");
   const { urls } = state.urlForm;
   const urlsLength = state.urlForm.urls.length;
 
@@ -257,7 +265,7 @@ export const render = (state) => {
     i18next,
     translate,
     state,
-    state.urlForm.linky,
+    state.urlForm.linky
   );
 };
 
